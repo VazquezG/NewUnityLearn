10961ec249acd41767c3158aa8109aed894cbb8a
VazquezG <vazquezguerra.oscar01@gmail.com>
2024-04-28 13:36:06 -0600

    Menu first commit
    Created the scene Menu and the Menu Manager script

---
+++ Assets/Scripts/ManagerMenu.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.UIElements;
+
+public class ManagerMenu : MonoBehaviour
+{
+    [SerializeField]
+    TMP_InputField NamePlayer;
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
